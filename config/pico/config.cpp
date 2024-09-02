bd5755319f6da4817596afacf3abf4db23db2d17
Tenshi159 <unspekabletbnrfrags@gmail.com>
2024-09-02 00:37:04 -0500

    Update config.cpp

--- config/pico/config.cpp
+++ config/pico/config.cpp
@@ -24,13 +24,13 @@ size_t backend_count;
 KeyboardMode *current_kb_mode = nullptr;
 
 GpioButtonMapping button_mappings[] = {
-    {&InputState::l,            5 },
+    {&InputState::mod_y,            5 },
     { &InputState::left,        4 },
     { &InputState::down,        3 },
     { &InputState::right,       2 },
 
     { &InputState::mod_x,       6 },
-    { &InputState::mod_y,       7 },
+    { &InputState::l,       7 },
 
     { &InputState::select,      10},
     { &InputState::start,       0 },
